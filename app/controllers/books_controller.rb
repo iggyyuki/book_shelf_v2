e2ae562e8a759e4541bb31cc1d848aa612b57396
Yuki <tigers24.backfrip.yuki1117@gmail.com>
2016-11-26 07:20:05 +0000

    バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,15 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
     
-    redirect_to books_path
+    if @book.valid?
+      @book.save
+      redirect_to books_path
+    else
+      flash[:alert] = @book.errors.full_messages
+      render :new
+    end
+    
   end
   
   def edit
